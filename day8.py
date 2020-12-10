d0e06a01e0a6af6f684b3d24f4bf28a96261c789
borkborkbork <jzoudavy@gmail.com>
2020-12-10 00:18:15 -0500

    day 8 f

--- day8.py
+++ day8.py
@@ -8,27 +8,49 @@ puzzle = Puzzle(year=2020, day=8)
 
 raw = puzzle.input_data
 data = raw.splitlines()
-data.append('')
+data.append('\n')
+
+def part1(data, i, loop_list, accumulator):
+    while True:
+        if 'nop' in data[i]:
+            i += 1
+        if 'acc' in data[i]:
+            accumulator += int(data[i].split(' ')[1])
+            i += 1
+        if 'jmp' in data[i]:
+            i = i + int(data[i].split(' ')[1])
+
+        if i in loop_list:
+            return False
+        if i == 622:
+            return True
+
+        loop_list.append(i)
+
+
+def part2(data):
+
+    for k,j in enumerate(data):
+        accumulator = 0
+        i = 0
+        loop_list = []
+
+        if 'nop' in j: j = j.replace('nop', 'jmp')
+        if 'jmp' in j: j = j.replace('jmp', 'nop')
+
+        data[k]=j
+        print('new list sent')
+        if (part1(data, i, loop_list, accumulator)): break
+        else:
+            print('shit '+str(k))
+
 
 accumulator = 0
 i = 0
 loop_list = []
-while True:
-    print(i)
-    print(data[i])
-    if i in loop_list:
 
-        break
-    loop_list.append(i)
-    if 'nop' in data[i]:
-        i += 1
-    if 'acc' in data[i]:
-        accumulator += int(data[i].split(' ')[1])
-        i += 1
-    if 'jmp' in data[i]:
-        i = i + int(data[i].split(' ')[1])
+#part1(data, i, loop_list, accumulator)
 
-print(loop_list)
-print(accumulator)
+part2(data)
 
 # submit(total_count, part="b", day=6, year=2020)
